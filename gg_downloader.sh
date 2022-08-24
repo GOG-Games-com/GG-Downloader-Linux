ee939ae035a069b82ad9e3282fefbc7a79e58b24
GG <109116372+GOG-Games-com@users.noreply.github.com>
2022-08-24 02:24:20 +0000

    formatting

--- gg_downloader.sh
+++ gg_downloader.sh
@@ -17,31 +17,28 @@ GAME_SLUG="$1"
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
-blue=$(tput setaf 4)
 reset=$(tput sgr0)
 
 ## Dependency checks
 # cURL 
 if ! command -v curl >/dev/null 2>&1; then
     echo ${red}"cURL in not installed."${reset} 
-	echo ${red}"sudo apt install curl"${reset} 
-	exit
+    echo ${red}"sudo apt install curl"${reset} 
+    exit
 fi
 
 # aria2 
 if ! command -v aria2c >/dev/null 2>&1; then
     echo ${red}"aria2 in not installed."${reset}
-	echo ${red}"sudo apt install aria2"${reset}
-	exit
+    echo ${red}"sudo apt install aria2"${reset}
+    exit
 fi
 
 # Make sure input is not empty
 if [  -z "$GAME_SLUG" ]; then
-
 	echo ${red}"Please supply a game slug."${reset}
 	echo ${red}"Exiting."${reset}
 	exit
-
 fi 
 
 # Scrape all file names from the game page
@@ -49,21 +46,17 @@ GAME_FILES=$(curl -s "$GG_BASE_URL/game/$GAME_SLUG" | grep -Po '(<span class=\"f
 
 # Make sure have file names before proceeding
 if [ -z "$GAME_FILES" ]; then
-
 	echo ${red}"No filenames were fetched from the game page."${reset}
 	echo ${red}"Please check your supplied game slug is valid, no filenames may also not listed on the game page or possibly connection to the website could not be made. "${reset}
 	echo ${red}"Exiting."${reset}
 	exit 
-
 fi 
 
 # Loop through and download each file
 
 while IFS= read -r file; do
-
 	echo ${green}"Downloading.. $file"${reset} 
 	aria2c --http-auth-challenge=true --http-user="$USERNAME" --http-passwd="$PASSWORD" -x "$DOWNLOAD_THREADS" -d "$DOWNLOAD_DIRECTORY/$GAME_SLUG" "$CDN_BASE_URL/downloads/$GAME_SLUG/$file"
-	
 done <<< "$GAME_FILES"
 
 exit
